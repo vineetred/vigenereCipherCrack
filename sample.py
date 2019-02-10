e18ddc47d1b5852f64af74a38cd3225178799a22
Vineet Reddy <hotshot2797@gmail.com>
2019-02-10 15:52:07 +0530

    Trying to crack it

---
+++ sample.py
@@ -0,0 +1,13 @@
+all_freq = {}
+# test_str = "qivjukosqegnyiytxypshzewjsnsdpeybsuiranshzewjsnsdvusdvozqhasghexhvtdrynjyirlrrnfpekjbsuhucnjyirlrrnfveylrsdgbinjyirlrrnfwilqbsuqlisfqhhzuxytxaewhroxwvasjirxwsltyiytxontzxhjuyljvenivsdtlectpqiypinylwwmdxirosoplrgkrvytxaoswkeywlixivordrytwlewjyynmysyzensdxeqocozkswnpjejomnlzensdqaphcozxrdjuwtfqhnjyirlrrnfjmvjbsuzsreahvgtqraqhxytxhobq" 
+file = open("7b.txt", "r") 
+hello = str(file.read())
+from collections import Counter 
+res = Counter(test_str) 
+print("HELLO")
+print(res)
+# import matplotlib.pyplot as plt
+# plt.plot(list(all_freq))
+# plt.show()
+
+# print(hello)
